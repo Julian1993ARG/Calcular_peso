acbba4f99180dcb393353770d3164c99cf814c09
Negocio <julianmartinez_93@hotmail.com>
2022-03-26 10:31:01 -0300

    finalizado estructura

---
+++ index.js
@@ -0,0 +1,13 @@
+let xx = document.getElementById("input");
+xx.addEventListener("click", calculo);
+
+
+
+function calculo(dato) {
+    
+    let x = document.getElementById("exampleInputEmail1");
+    let peso = x.value;
+    alert('Tu peso es de kg ' + peso );
+}
+//console.log(number)
+
